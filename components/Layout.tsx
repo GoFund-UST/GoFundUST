cce8d680b8ec2cf2c92e00125db52bd5cb02787c
Davide <davidediak@gmail.com>
2022-02-10 22:43:43 +0100

    refactor: removed unused hooks & some fns, some minor edits

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,11 @@
-import React, { FC } from "react";
-import { Box, Flex } from "@chakra-ui/react";
-import { Global } from "@emotion/react";
-import { TerraWebappProvider } from "@arthuryeti/terra";
-import { useWallet, WalletStatus } from "@terra-money/wallet-provider";
+import {TerraWebappProvider} from '@arthuryeti/terra';
+import {Box, Flex} from '@chakra-ui/react';
+import {Global} from '@emotion/react';
+import {useWallet, WalletStatus} from '@terra-money/wallet-provider';
+import Navbar from 'components/Navbar';
+import React, {FC} from 'react';
 
-import Navbar from "components/Navbar";
-import whitelist from "constants/whitelist.json";
-import { AstroAppProvider } from "modules/common";
-
-const Layout: FC = ({ children }) => {
+const Layout: FC = ({children}) => {
   const wallet = useWallet();
   const isInitializing = wallet.status === WalletStatus.INITIALIZING;
 
@@ -16,40 +13,38 @@ const Layout: FC = ({ children }) => {
     <Flex height="100vh" direction="column">
       <Global
         styles={{
-          "html,body": {
+          'html,body': {
             // height: "100%",
-            width: "100%",
-            overflowX: "hidden",
-            position: "relative",
+            width: '100%',
+            overflowX: 'hidden',
+            position: 'relative',
           },
           body: {
-            backgroundColor: "#000D37",
+            backgroundColor: '#000D37',
           },
-          "*::-webkit-scrollbar": {
-            width: "6px",
+          '*::-webkit-scrollbar': {
+            width: '6px',
           },
-          "*::-webkit-scrollbar-thumb": {
-            backgroundColor: "#5643F2",
-            borderRadius: "6px",
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#5643F2',
+            borderRadius: '6px',
           },
-          "#chakra-toast-manager-bottom-right": {
-            right: "32px!important",
-            bottom: "32px!important",
+          '#chakra-toast-manager-bottom-right': {
+            right: '32px!important',
+            bottom: '32px!important',
           },
-          "@font-face": {
-            fontFamily: "WhyteInktrap",
+          '@font-face': {
+            fontFamily: 'WhyteInktrap',
             src: "url('/WhyteInktrap-Regular.woff') format('woff')",
           },
         }}
       />
       {!isInitializing && (
         <TerraWebappProvider>
-          <AstroAppProvider data={whitelist}>
-            <Box>
-              <Navbar />
-            </Box>
-            <Box flex="1">{children}</Box>
-          </AstroAppProvider>
+          <Box>
+            <Navbar />
+          </Box>
+          <Box flex="1">{children}</Box>
         </TerraWebappProvider>
       )}
     </Flex>
