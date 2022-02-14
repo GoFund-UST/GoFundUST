4f3bddffd3599fca6a1a43b5b1fd0966232c2238
Davide <davidediak@gmail.com>
2022-02-14 07:47:16 +0100

    style: fixed a bit fund detail's layout

--- components/FundDetail.tsx
+++ components/FundDetail.tsx
@@ -2,25 +2,20 @@ import {useAddress} from '@arthuryeti/terra';
 import {
   Box,
   Button,
+  Divider,
   Flex,
   FormControl,
   FormErrorMessage,
   FormLabel,
-  HStack,
-  IconButton,
   Input,
   Link,
   Text,
-  VStack,
 } from '@chakra-ui/react';
 import {TxResult, useConnectedWallet} from '@terra-money/wallet-provider';
 import Card from 'components/Card';
 import FundAmountCard from 'components/common/FundAmountCard';
 import PageLoading from 'components/common/PageLoading';
-import CloseModalIcon from 'components/icons/CloseModalIcon';
-import SuccessIcon from 'components/icons/SuccessIcon';
 import {motion} from 'framer-motion';
-import {truncate} from 'libs/text';
 import {CrowdFundConfigResponse, CrowdFundStateResponse} from 'modules/crowdfund';
 import {
   useCrowdFundDepositPool,
@@ -42,6 +37,7 @@ const MotionBox = motion(Box);
 
 const FundDetail: FC<Props> = ({detail, claimable, address, onCloseClick}) => {
   const userAddress = useAddress();
+  const connectedWallet = useConnectedWallet();
   const depositPoolDetails = useCrowdFundDepositPool(detail.dp_token);
   const depositPoolBalance = useCrowdFundDepositPoolBalance(detail.dp_token, userAddress);
   const [txResult, setTxResult] = useState<TxResult | null>(null);
@@ -52,8 +48,6 @@ const FundDetail: FC<Props> = ({detail, claimable, address, onCloseClick}) => {
       amount: 0,
     },
   });
-
-  const connectedWallet = useConnectedWallet();
 
   const {submit: submitContribution} = usePostContributeFund({
     fundAmount: form.watch('amount'),
@@ -101,82 +95,55 @@ const FundDetail: FC<Props> = ({detail, claimable, address, onCloseClick}) => {
         m="0 auto"
         mt="10">
         <Card>
-          <Flex justify="space-between" align="center" mb="4">
-            <HStack>
-              <SuccessIcon />
-              <Text fontSize="lg" color="green.500">
-                {detail.pool_title}
-              </Text>
-            </HStack>
-            <IconButton
-              aria-label="Close"
-              icon={<CloseModalIcon w="1.5rem" h="1.5rem" />}
-              variant="icon"
-              onClick={onCloseClick}
-            />
-          </Flex>
-          <Text fontSize="light" color="green.500">
-            beneficiary{' '}
-            <a
-              href={'https://finder.extraterrestrial.money/testnet/address/' + detail.beneficiary}
-              target="_beneficiary">
-              {detail.beneficiary}
-            </a>
-          </Text>
-          <pre>{detail.pool_description}</pre>
-
           <FundAmountCard
-            token={detail.dp_token}
-            money_market={detail.money_market}
+            detail={detail}
             claimable={claimable}
-            description={truncate(address)}
-            token_details={depositPoolDetails.data}
             account_details={depositPoolBalance.data}
           />
-          <VStack>
-            <HStack>
-              <VStack>
-                <form color="black">
-                  <label>$UST</label>
-                  <FormControl mt={4} isInvalid={!!form.formState.errors.amount}>
-                    <FormLabel htmlFor="amount">Fund name</FormLabel>
-                    <Input
-                      type="number"
-                      id="amount"
-                      {...form.register('amount', {
-                        required: 'This is required',
-                        min: {value: 0.1, message: 'Min value is 0.1'},
-                        valueAsNumber: true,
-                      })}
-                    />
-                    <FormErrorMessage>{form.formState.errors?.amount?.message}</FormErrorMessage>
-                  </FormControl>
-                  <Button
-                    variant="primary"
-                    width="256px"
-                    disabled={!form?.formState?.isValid}
-                    onClick={e => {
-                      e.preventDefault();
-                      fundProject();
-                    }}>
-                    Fund
-                  </Button>
-                  {depositPoolBalance.data.balance > 0 && (
-                    <Button
-                      variant="primary"
-                      width="256px"
-                      disabled={!form?.formState?.isValid}
-                      onClick={e => {
-                        e.preventDefault();
-                        redeem();
-                      }}>
-                      Redeem
-                    </Button>
-                  )}
-                </form>
-              </VStack>
-            </HStack>
-            {connectedWallet && txResult && (
+          <Divider mt="8" mb="8" />
+          <form>
+            <FormControl isInvalid={!!form.formState.errors.amount}>
+              <FormLabel htmlFor="amount">Amount ($UST)</FormLabel>
+              <Input
+                type="number"
+                id="amount"
+                {...form.register('amount', {
+                  required: 'This is required',
+                  min: {value: 0.1, message: 'Min value is 0.1'},
+                  valueAsNumber: true,
+                })}
+              />
+              <FormErrorMessage>{form.formState.errors?.amount?.message}</FormErrorMessage>
+            </FormControl>
+            <Flex justifyContent="space-evenly">
+              <Button
+                variant="primary"
+                width="256px"
+                m="20px"
+                disabled={!form?.formState?.isValid}
+                onClick={e => {
+                  e.preventDefault();
+                  fundProject();
+                }}>
+                Fund
+              </Button>
+              {depositPoolBalance.data.balance > 0 && (
+                <Button
+                  variant="primary"
+                  width="256px"
+                  m="20px"
+                  disabled={!form?.formState?.isValid}
+                  onClick={e => {
+                    e.preventDefault();
+                    redeem();
+                  }}>
+                  Redeem
+                </Button>
+              )}
+            </Flex>
+          </form>
+          {connectedWallet && txResult && (
+            <Flex justifyContent="center">
               <Text>
                 <Link
                   href={`https://finder.terra.money/${connectedWallet.network.chainID}/tx/${txResult.result.txhash}`}
@@ -186,9 +153,9 @@ const FundDetail: FC<Props> = ({detail, claimable, address, onCloseClick}) => {
                   Open Tx Result in Terra Finder
                 </Link>
               </Text>
-            )}
-            {txError && <Text color="red.500">{txError}</Text>}
-          </VStack>
+            </Flex>
+          )}
+          {txError && <Text color="red.500">{txError}</Text>}
         </Card>
       </MotionBox>
     );
