c28e9b1fc7c4db99379b15091ec8a67ff5420aa4
Davide <davidediak@gmail.com>
2022-02-17 18:18:20 +0100

    fix: lib -> included theme

--- lib/src/ContributeToFund.tsx
+++ lib/src/ContributeToFund.tsx
@@ -1,16 +1,20 @@
+import {Text, ThemeProvider} from '@chakra-ui/react';
 import React from 'react';
-import {Text} from '@chakra-ui/react';
+import theme from '../../theme';
 
 const ContributeToFund: React.FC<{fundAddress: string}> = ({fundAddress}) => {
+  const id = 'go-fund-ust-contribute-to-fund';
   return (
-    <>
-      <Text fontSize="3xl">
-        Fund Address
-      </Text>
-      <Text variant="cardDescription" fontSize="xl">
-        {fundAddress}
-      </Text>
-    </>
+    <div id={id}>
+      <ThemeProvider theme={theme} cssVarsRoot={`#${id}`}>
+        <>
+          <Text fontSize="3xl">Fund Address</Text>
+          <Text variant="cardDescription" fontSize="xl">
+            {fundAddress}
+          </Text>
+        </>
+      </ThemeProvider>
+    </div>
   );
 };
 
