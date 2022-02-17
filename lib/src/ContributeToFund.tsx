13f017cf2a2273422b86e9b2bb02ce57fe625686
Davide <davidediak@gmail.com>
2022-02-17 21:08:05 +0100

    style: lib ContributeToFund

--- lib/src/ContributeToFund.tsx
+++ lib/src/ContributeToFund.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Card from '../../components/Card';
 import {useCrowdFund} from '../../modules/crowdfund';
 import LibWrapper from './util/LibWrapper';
-import {useWallet} from '@terra-money/wallet-provider';
 import {Wallet} from '@terra-money/terra.js';
 
 const ContributeToFund: React.FC<{fundAddress: string; wallet: Wallet}> = ({
@@ -19,22 +18,27 @@ const ContributeToFund: React.FC<{fundAddress: string; wallet: Wallet}> = ({
 
 const Component: React.FC<{fundAddress: string; wallet: Wallet}> = ({fundAddress, wallet}) => {
   const {isLoading, data} = useCrowdFund(fundAddress);
-  console.log(wallet?.network?.name);
 
   return (
     <>
-      <Card>
-        <Text fontSize="3xl">Fund Address</Text>
-        <Text variant="cardDescription" fontSize="xl">
-          {fundAddress}
-        </Text>
-        <Text variant="content" fontSize="3xl">
-          Pool Name
-        </Text>
-        <Text variant="cardDescription" fontSize="xl">
-          {data?.pool_name}
-        </Text>
-      </Card>
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          overflowX: 'hidden',
+          position: 'relative',
+          backgroundColor: '#000D37',
+          borderRadius: "1rem"
+        }}>
+        <Card>
+          <Text variant="content" fontSize="3xl">
+            Pool Name
+          </Text>
+          <Text variant="cardDescription" fontSize="xl">
+            {data?.pool_name}
+          </Text>
+        </Card>
+      </div>
     </>
   );
 };
