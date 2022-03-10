8a0f6f8f67bcc02ec6686bcbf5184885c12c5d61
Go Fund UST <100543132+GoFundUST-Developer@users.noreply.github.com>
2022-03-10 14:13:43 +1100

    add warning message

--- pages/fund/[address].tsx
+++ pages/fund/[address].tsx
@@ -1,9 +1,10 @@
-import {Container} from '@chakra-ui/react';
+import {Box, Container, Text} from '@chakra-ui/react';
 import Disclaimer from 'components/Disclaimer';
 import Fund from 'components/pages/Fund';
 import {NextPage} from 'next';
 import Head from 'next/head';
 import React from 'react';
+import Card from "components/Card";
 
 const FundPage: NextPage = () => {
   return (
@@ -12,6 +13,13 @@ const FundPage: NextPage = () => {
         <title>Fund</title>
       </Head>
       <Container my="12" px={['6', null, '12']} position="relative" maxWidth="container.xl">
+          <Card p={['12', null, null, '12']} w={'100%'} bg='white' color='red'>
+              <Box>
+                  <Text size='lg'><b>Warning</b></Text>
+                  <Text> Anyone can create a fund claiming they are whomever they want to be.</Text>
+                  <Text> Please use links provided by reputable people on their own pages, and don&apos;t just blindly ape into any you find here</Text>
+              </Box>
+          </Card>
         <Fund />
         <Disclaimer />
       </Container>
